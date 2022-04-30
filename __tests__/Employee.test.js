a4626f40bcf3556c0aa94f487388db42c4a94d69
jennasaikaly <jen.saikaly@gmail.com>
2022-04-30 18:28:12 -0400

    wrote tests and methods for employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,27 +1,32 @@
 const Employee = require('../lib/Employee');
 jest.mock('../lib/Employee.js');
-//test('creates an Employee object', () => {
-  //  const employee = new Employee('Jenna');
 
-    //expect(employee.name).toBe('Jenna');
-    //expect(employee.id).toEqual(expect.any(Number));
-   // expect(employee.email).toEqual(expect.any(Object));
-    //});
 
-    test ('gets employee name', () => {
-        const employee = new Employee('');
+// test('creates an Employee object', () => {
+//    const employee = new Employee('Jenna');
 
+//    expect(employee.name).toBe('Jenna');
+//    expect(employee.id).toEqual(expect.any(String));
+//    expect(employee.email).toEqual(expect.any(String));
+//    expect(employee.role).toBe(String);
+//     });
+
+test('gets employee name', () => {
+    const employee = new Employee('');
     expect(employee.name).toEqual(expect.any(String));
-    })
+})
 
-    test ('gets employee ID number', () => {
-        const employee = new Employee('');
-
+test('gets employee ID number', () => {
+    const employee = new Employee('');
     expect(employee.id).toEqual(expect.any(String));
-    })
+})
 
-    test ('gets employee email address', () => {
-        const employee = new Employee('');
-
+test('gets employee email address', () => {
+    const employee = new Employee('');
     expect(employee.id).toEqual(expect.any(String));
-    })
+})
+
+test('gets employee role', () => {
+    const employee = new Employee('');
+    expect(employee.role).toBe('Employee');
+})
---
+++ index.js
@@ -0,0 +1,71 @@
+{
+    inquirer
+    .prompt({
+        type: 'text',
+        name: 'name',
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name.');
+                return false;
+            }
+        }
+    })
+    //destructure name from the prompt object
+    //using arrow key to bc of lexical scope needed for 'this' to refer to object
+    .then(({ name }) =>{
+        this.employee = new Employee(name);
+//test object creation
+console.log(this.employee);            
+    })
+}    
+
+ getID = function(){
+    inquirer
+    .prompt({
+        type: 'text',
+        name: 'id',
+        message: 'What is your ID number?',
+        validate: idInput => {
+            if (idInput) {
+              return true;
+            } else {
+              console.log('Please enter your id number.');
+              return false;
+            }
+          }
+    })
+    //destructure id from the prompt object
+    //using arrow key to bc of lexical scope needed for 'this' to refer to object
+    .then(({ id }) =>{
+        this.employee = new Employee(id);
+//test object creation
+console.log(this.employee);            
+    })
+}
+
+getEmail = function(){
+    inquirer
+    .prompt({
+        type: 'text',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter your email address.');
+              return false;
+            }
+          }
+    })
+    //destructure id from the prompt object
+    //using arrow key to bc of lexical scope needed for 'this' to refer to object
+    .then(({ email }) =>{
+        this.employee = new Employee(email);
+//test object creation
+console.log(this.employee);            
+    })
+}
