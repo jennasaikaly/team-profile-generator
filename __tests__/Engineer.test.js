f751e9b7a8b118cb3b1f2e2adbb7f3f9a925e970
jennasaikaly <jen.saikaly@gmail.com>
2022-04-30 18:53:12 -0400

    added constructors, tests and methods for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,13 @@
 
 const Engineer = require('../lib/Engineer');
 jest.mock('../lib/Engineer.js');
+
+test('gets GitHub account name', () => {
+    const engineer = new Engineer('');
+    expect(engineer.github).toEqual(expect.any(String));
+    })
+
+test('gets role', () => {
+    const engineer = new Engineer('');
+    expect(engineer.role).toBe('Engineer');
+    })  
