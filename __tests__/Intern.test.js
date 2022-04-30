a0cfc38abd5315a397ae9899b5b5d2e026a26a2e
jennasaikaly <jen.saikaly@gmail.com>
2022-04-30 18:58:21 -0400

    added constructor, tests, and methods for intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,12 @@
 const Intern = require('../lib/Intern');
 jest.mock('../lib/Intern.js');
+
+test('gets school name', () => {
+    const intern = new Intern('');
+    expect(intern.school).toEqual(expect.any(String));
+    })
+
+test('gets role', () => {
+    const intern = new Intern('');
+    expect(intern.role).toBe('Intern');
+    })
